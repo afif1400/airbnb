0d892e21ab24366df91ff6ae1d1db3a46825459a
afif ahmed <afifahmed456123@gmail.com>
2021-03-14 20:59:56 +0530

    amplify init

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import "react-native-gesture-handler";
 import React from "react";
 import Router from "./src/navigation/Router";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 export default function App() {
   return (
